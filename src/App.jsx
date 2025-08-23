078bd33b030e37dde53968316e9107744d3db662
Nguyen Thanh Tung <tungnguyenthanh710@gmail.com>
2025-08-23 21:28:42 +0700

    vid 47

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './components/todo/todo.css';
 import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
 
 const App = () => {
 
@@ -13,18 +15,22 @@ const App = () => {
   }
 
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <TodoNew />
-      <TodoData
-        name={hoidanit}
-        age={age}
-        data={data}
-      />
-      <div className="todo-image">
-        <img src={reactLogo} alt="" className="logo" />
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew />
+        <TodoData
+          name={hoidanit}
+          age={age}
+          data={data}
+        />
+        <div className="todo-image">
+          <img src={reactLogo} alt="" className="logo" />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   )
 }
 
